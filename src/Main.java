80de7a61862f3b5ddbb382b6674ef5ac360a8458
s30855mariakibets <s30855@pjwstk.edu.pl>
2024-10-09 11:23:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+
 public class Main {
+    // Class to perform addition
+class Adder {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+// Class to perform subtraction
+class Subtractor {
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
     public static void main ( String [] args ) {
         Adder adder = new Adder () ;
         System . out . println ( adder . add (1 , 2) ) ;
